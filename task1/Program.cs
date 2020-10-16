2f8527a253dfcb99a729ddb2703641f0dace07de
Justice Okpala <justicecharles497@gmail.com>
2020-10-16 08:21:35 +0100

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter your name: ");
+         string name = Console.ReadLine();
+
+         Console.WriteLine("enter your age");
+         string age = Console.ReadLine();
+
+         Console.WriteLine($"your name is {name} and age is {age}");
+
+
+
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*");
+             Console.WriteLine("* *");
+              Console.WriteLine("***");
+ 
+        }
+    } 
+}
